e9b832fe61c5b78d6fe44a87d4b64013cb8005e7
Leonardo Bastos <leosbastos7@gmail.com>
2021-01-12 18:26:30 +0000

    REST API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const fakeHMR = require('./fake-hmr');
-
+const fs = require('fs');
 const compiler = webpack(webpackConfig);
 
 const watching = compiler.watch({
@@ -25,23 +25,85 @@ const watching = compiler.watch({
 
 const app = express();
 fakeHMR.config({ app });
+app.use(express.json())
 app.use(express.static('public'));
-
 // require('./webpackRunner');
 
-app.get('*', (req, res) => {
-  res.send(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>hi</title>
-  </head>
-  <body>
-    <div id="app"/>
-    <script src="/bundle.js" type="text/javascript"></script>
-  </body>
-</html>`)
+// Codigo criado
+let anime;
+//Escrita de arquivo JSON
+function writeJson() {
+  fs.writeFileSync(__dirname + '/animes.json', JSON.stringify(anime))
+  console.log("Arquivo JSON atualizado!")
+}
+//Leitura de arquivo JSON
+function getAnime() {
+  anime = JSON.parse(fs.readFileSync(__dirname + '/animes.json', 'utf-8'))
+}
+
+//Adiciona novas informações
+function postAnime(animeData) {
+  anime.animes.push(
+    animeData
+  )
+  console.log(anime)
+  writeJson();
+}
+
+//Atualiza os dados de acordo com o id econtrado
+function putAnime(animeData, id) {
+  anime.animes = anime.animes.map(el => {
+    console.log(el.id)
+    if (el.id === id) {
+      console.log('teste')
+      return animeData
+    }
+    return el
+  });
+  console.log(anime)
+  writeJson();
+}
+
+//Busca o id numa lista e retorna uma lista sem ele
+function deleteAnime(id) {
+  anime.animes = anime.animes.filter(el => { return id !== el.id })
+  writeJson();
+}
+
+app.get('/', (req, res) => {
+  getAnime();
+  res.json(anime);
 });
 
-app.listen(config.PORT, function () {
+app.post('/', (req, res) => {
+  getAnime();
+  postAnime(req.body);
+  res.status(200).send("Adicionado com sucesso");
+})
+
+app.put('/:id', (req, res) => {
+  getAnime();
+  putAnime(req.body, req.params.id);
+  res.status(200).send("Editado com sucesso");
+})
+
+app.delete('/:id', (req, res) => {
+  getAnime();
+  deleteAnime(req.params.id);
+  res.status(200).send("Deletado com sucesso");
+})
+
+app.get('/season', (req, res) => {
+  fs.readFile(__dirname + '/animes-season.json', 'utf-8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(
+      JSON.parse(data)
+    )
+  })
+});
+
+app.listen(config.PORT, function() {
   console.log(`App currently running; navigate to localhost:${config.PORT} in a web browser.`);
 });
---
+++ knexfile.js
@@ -0,0 +1,11 @@
+impor path = require('path')
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  }
+}
